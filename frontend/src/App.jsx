7528f791b8e5a428a6c0a37aef1c91553792aabc
Muhammad <muhammadnashef2@gmail.com>
2026-01-03 01:10:04 +0200

    fix scroll placement

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,19 +9,32 @@ import Layout from "../src/layouts/Layout.jsx";
 // Route Protection
 import AdminRoute from "./routes/AdminRoute";
 
+import ScrollToTop from "./components/ScrollToTop";
+
 function App() {
   return (
-    <Routes>
-      {/* Public Layout*/}
-      <Route element={<Layout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/sub-categories/:mainCategoryId" element={<SubCategories />} />
-        <Route path="/questions/:subCategoryId" element={<Questions />} />
-        {/* Protected Admin Dashboard */}
-        <Route path="/admin-dashboard" element={<AdminRoute><AdminDashboard /></AdminRoute>}/>
+    <BrowserRouter>
+      <ScrollToTop />
+
+      <Routes>
+        {/* Public Layout */}
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/sub-categories/:mainCategoryId" element={<SubCategories />} />
+          <Route path="/questions/:subCategoryId" element={<Questions />} />
+
+          {/* Protected Admin Dashboard */}
+          <Route
+            path="/admin-dashboard"
+            element={
+              <AdminRoute>
+                <AdminDashboard />
+              </AdminRoute>
+            }
+          />
         </Route>
-    </Routes>
-      
+      </Routes>
+    </BrowserRouter>
   );
 }
 
