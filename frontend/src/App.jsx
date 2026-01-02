fb70eae436c0490e2e1e06e58ab0395e8d57df99
Muhammad <muhammadnashef2@gmail.com>
2026-01-03 01:11:16 +0200

    add browser router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import {BrowserRouter, Routes, Route } from "react-router-dom";
 // Pages
 import Home from "./pages/Home";
 import Questions from "./pages/Questions";
